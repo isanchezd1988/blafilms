6065590ad7e16bed904e9e3565df721709b156fb
Juan Redradix <juanportillo@redradix.com>
2022-02-25 13:30:05 +0100

    :white_check_mark: Renders default text on empty results

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('fails', () => {
+  it('renders default text on empty results', () => {
     render(<App />)
 
     expect(screen.getByText('No results yet')).toBeInTheDocument()
