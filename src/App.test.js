94e3f95fd8af7cba4365546bbec0bfd1798e636c
Ines <ines@redradix.com>
2022-02-25 13:50:30 +0100

    tests: search button only fires action when query is not empty

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 
 import App from './App'
+import InputSearch from './InputSearch'
 
 describe('App', () => {
   it('fails', () => {
@@ -12,7 +13,7 @@ describe('App', () => {
   it('renders', () => {
     render(<App />)
 
-    expect(screen.getByPlaceholderText('Search')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Search...')).toBeInTheDocument()
     expect(screen.getByText('Search')).toBeInTheDocument()
   })
 })
@@ -21,10 +22,37 @@ describe('Input value', () => {
   it('value updates on change', () => {
     render(<App />)
 
-    const searchInput = screen.getByPlaceholderText('Search')
+    const searchInput = screen.getByPlaceholderText('Search...')
 
     fireEvent.change(searchInput, { target: { value: 'test' } })
 
     expect(searchInput.value).toBe('test')
+  })
+})
+
+describe('Search button', () => {
+  it('when no query, it will not trigger api request', () => {
+    const apiRequest = jest.fn()
+
+    render(<InputSearch searchAction={apiRequest} />)
+
+    const button = screen.getByText('Search')
+
+    fireEvent.click(button)
+
+    expect(apiRequest).not.toHaveBeenCalled()
+  })
+  it('when query is not empty, it will trigger api request', () => {
+    const apiRequest = jest.fn()
+
+    render(<InputSearch searchAction={apiRequest} />)
+
+    const searchInput = screen.getByPlaceholderText('Search...')
+    const button = screen.getByText('Search')
+
+    fireEvent.change(searchInput, { target: { value: 'test' } })
+    fireEvent.click(button)
+
+    expect(apiRequest).toHaveBeenCalled()
   })
 })
