fa2f015ca7b9ab1f4e7c1c28e66ef9955fb4499c
Juan Redradix <juanportillo@redradix.com>
2022-02-25 13:09:37 +0100

    :art: Move Card block to its own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import placeholderImg from './placeholder.png'
+import Card from './components/Card'
 import { ReactComponent as ChevronLeft } from './chevron-left.svg'
 import { ReactComponent as ChevronRight } from './chevron-right.svg'
 import { getMovies } from './services'
 
-function App() {
+const App = () => {
   const [searchQuery, setSearchQuery] = useState('')
   const [searchResult, setSearchResult] = useState(null)
 
@@ -42,16 +42,7 @@ function App() {
           </div>
           <div className="search-results-list">
             {searchResult.Search.map(result => (
-              <div key={result.imdbID} className="search-item">
-                <img
-                  src={result.Poster === 'N/A' ? placeholderImg : result.Poster}
-                  alt="poster"
-                />
-                <div className="search-item-data">
-                  <div className="title">{result.Title}</div>
-                  <div className="meta">{`${result.Type} | ${result.Year}`}</div>
-                </div>
-              </div>
+              <Card result={result} key={result.imdbID} />
             ))}
           </div>
           <div className="chevron">
