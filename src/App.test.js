732cc1dfc34888ddc40b6fb3d6a64b0a0d3a6799
Aarón Contreras <aaron@redradix.com>
2022-02-25 12:24:21 +0100

    Update message for initial test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('fails', () => {
+  it('renders a message while data is fetching', () => {
     render(<App />)
 
     expect(screen.getByText('No results yet')).toBeInTheDocument()
