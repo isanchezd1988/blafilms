95b0cd496c68eca65541fc948ac17d605184831f
Roberto Redradix <roberto@redradix.com>
2022-02-25 13:43:42 +0100

    [Red] Test for a list of movies

--- src/App.test.js
+++ src/App.test.js
@@ -9,5 +9,19 @@ describe('App', () => {
 
       expect(screen.getByText('No results yet')).toBeInTheDocument()
     })
+
+    it("doesn't show a list of movies", () => {
+      render(<App />)
+
+      expect(screen.queryByRole('list')).toBeNull()
+    })
+  })
+
+  describe('when loaded', () => {
+    it('shows a default movies list', async () => {
+      render(<App />)
+
+      expect(await screen.findByRole('list')).toBeInTheDocument()
+    })
   })
 })
