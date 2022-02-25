087bce3a06c3ee4ac47eaf13be64fded118bb6cb
Aarón Contreras <aaron@redradix.com>
2022-02-25 12:25:58 +0100

    Add test for film rendering after request fulfills

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 import App from './App'
 
@@ -7,5 +7,16 @@ describe('App', () => {
     render(<App />)
 
     expect(screen.getByText('No results yet')).toBeInTheDocument()
+  })
+
+  it('after data is fetched, renders a grid of films', async () => {
+    render(<App />)
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('The Lord of the Rings: The Return of the King'),
+      ).toBeInTheDocument()
+      expect(screen.getByText('King Kong')).toBeInTheDocument()
+    })
   })
 })
