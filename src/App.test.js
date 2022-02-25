81c604d98af626249d54028b775f0576e1753af7
Roberto Redradix <roberto@redradix.com>
2022-02-25 12:46:42 +0100

    Group test by context

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,11 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('starts without preloaded films', () => {
-    render(<App />)
+  describe('when just mounted', () => {
+    it('shows a default message', () => {
+      render(<App />)
 
-    expect(screen.getByText('No results yet')).toBeInTheDocument()
+      expect(screen.getByText('No results yet')).toBeInTheDocument()
+    })
   })
 })
