cd0b0053acb92d29a70cc0254ac4de120944bfd0
Maria G Lomana <maria@redradix.com>
2022-02-28 08:42:18 +0100

    Add dynamic search and pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import cx from 'classnames'
+
 import './App.css'
 import placeholderImg from './placeholder.png'
 import { ReactComponent as ChevronLeft } from './chevron-left.svg'
@@ -7,53 +9,78 @@ import { ReactComponent as ChevronRight } from './chevron-right.svg'
 import { fetchResults } from './service'
 
 function App() {
-  const [searchResult, setSearchResult] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  const [page, setPage] = useState(1)
+  const [numberOfPages, setNumberOfPages] = useState()
+  const [results, setResults] = useState([])
+
+  const handleSearch = useCallback(async (searchInput, page) => {
+    const response = await fetchResults(searchInput, page)
+    const results = response?.results ?? []
+    setResults(results)
+    const numberOfPages = response?.numberOfPages ?? undefined
+    setNumberOfPages(numberOfPages)
+  }, [])
+
+  const handleFormSubmit = async ev => {
+    ev.preventDefault()
+    handleSearch(searchInput, page)
+  }
 
   useEffect(() => {
-    const fetch = async () => {
-      const results = await fetchResults('king')
-
-      if (results) {
-        setSearchResult(results)
-      }
-    }
-
-    fetch()
-  }, [])
+    handleSearch(searchInput, page)
+  }, [page, handleSearch])
 
   return (
     <div className="App">
-      <div className="search">
-        <input type="text" placeholder="Search..." />
-        <button>Search</button>
-      </div>
-      {!searchResult ? (
-        <p>No results yet</p>
+      <form className="search" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchInput}
+          onChange={ev => setSearchInput(ev.target.value)}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </form>
+      {results.length === 0 ? (
+        <div className="search-no-results">
+          <p>No results yet</p>
+        </div>
       ) : (
         <div className="search-results">
-          <div className="chevron">
+          <button
+            aria-label="prev-page"
+            className={cx('chevron', page === 1 ? 'is-disabled' : 'is-active')}
+            onClick={() => page > 1 && setPage(page - 1)}
+          >
             <ChevronLeft />
-          </div>
-          <div className="search-results-list">
-            {searchResult.length !== 0 &&
-              searchResult.map(result => (
-                <div key={result.imdbID} className="search-item">
-                  <img
-                    src={
-                      result.Poster === 'N/A' ? placeholderImg : result.Poster
-                    }
-                    alt="poster"
-                  />
-                  <div className="search-item-data">
-                    <div className="title">{result.Title}</div>
-                    <div className="meta">{`${result.Type} | ${result.Year}`}</div>
-                  </div>
+          </button>
+          <ul className="search-results-list">
+            {results.map(result => (
+              <li key={result.imdbID} className="search-item">
+                <img
+                  src={result.Poster === 'N/A' ? placeholderImg : result.Poster}
+                  alt="poster"
+                />
+                <div className="search-item-data">
+                  <div className="title">{result.Title}</div>
+                  <div className="meta">{`${result.Type} | ${result.Year}`}</div>
                 </div>
-              ))}
-          </div>
-          <div className="chevron">
+              </li>
+            ))}
+          </ul>
+          <button
+            aria-label="next-page"
+            className={cx(
+              'chevron',
+              page === numberOfPages ? 'is-disabled' : 'is-active',
+            )}
+            onClick={() => page < numberOfPages && setPage(page + 1)}
+          >
             <ChevronRight />
-          </div>
+          </button>
         </div>
       )}
     </div>
--- src/service.js
+++ src/service.js
@@ -1,14 +1,19 @@
 const API_URL = process.env.REACT_APP_API_URL
 const API_KEY = process.env.REACT_APP_API_KEY
+const NUMBER_OF_RESULTS_PER_PAGE = 10
 
 export const fetchResults = async (searchInput, page = 1) => {
   try {
-    const results = await fetch(
+    const response = await fetch(
       `${API_URL}/?apikey=${API_KEY}&s=${searchInput}&page=${page}`,
     ).then(res => res.json())
 
-    if (results.Response === 'True') {
-      return results.Search
+    if (response.Response === 'True') {
+      const results = response.Search
+      const numberOfPages = Math.ceil(
+        response.totalResults / NUMBER_OF_RESULTS_PER_PAGE,
+      )
+      return { results, numberOfPages }
     }
   } catch (e) {
     console.error(e)
