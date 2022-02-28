031a334f9c8897f9ce5daf99ecf0e3e71ccb87b9
Maria G Lomana <maria@redradix.com>
2022-02-28 08:56:03 +0100

    :white_check_mark: 'It renders the initial page' test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react'
-
 import App from './App'
 
 describe('App', () => {
-  it('fails', () => {
+  it('renders the initial page', () => {
     render(<App />)
 
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
     expect(screen.getByText('No results yet')).toBeInTheDocument()
+    expect(screen.queryByRole('prev-page')).toBeNull()
+    expect(screen.queryByRole('next-page')).toBeNull()
   })
 })
