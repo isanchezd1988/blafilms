26fb9506eb50f8f712847551cecba62ac0a5638b
Andres Martin Angulo <monkeyandres@protonmail.com>
2022-03-02 17:00:01 +0100

    Move SearchResults into a separate component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
-import placeholderImg from './assets/placeholder.png'
-import { ReactComponent as ChevronLeft } from './assets/chevron-left.svg'
-import { ReactComponent as ChevronRight } from './assets/chevron-right.svg'
+import SearchResults from './SearchResults'
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('')
@@ -41,32 +39,7 @@ function App() {
         <button onClick={performSearch}>Search</button>
       </div>
 
-      {!searchResult ? (
-        <p>No results yet</p>
-      ) : (
-        <div className="search-results">
-          <div className="chevron">
-            <ChevronLeft />
-          </div>
-          <div className="search-results-list">
-            {searchResult.Search.map(result => (
-              <div key={result.imdbID} className="search-item">
-                <img
-                  src={result.Poster === 'N/A' ? placeholderImg : result.Poster}
-                  alt="poster"
-                />
-                <div className="search-item-data">
-                  <div className="title">{result.Title}</div>
-                  <div className="meta">{`${result.Type} | ${result.Year}`}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="chevron">
-            <ChevronRight />
-          </div>
-        </div>
-      )}
+      <SearchResults searchResults={searchResult} />
     </div>
   )
 }
---
+++ src/SearchResults.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './SearchResults.css'
+import placeholderImg from './assets/placeholder.png'
+import { ReactComponent as ChevronLeft } from './assets/chevron-left.svg'
+import { ReactComponent as ChevronRight } from './assets/chevron-right.svg'
+
+const SearchResults = ({ searchResults }) => {
+  return !searchResults ? (
+    <p>No results yet</p>
+  ) : (
+    <div className="search-results">
+      <div className="chevron">
+        <ChevronLeft />
+      </div>
+      <div className="search-results-list">
+        {searchResults.Search.map(result => (
+          <div key={result.imdbID} className="search-item">
+            <img
+              src={result.Poster === 'N/A' ? placeholderImg : result.Poster}
+              alt="poster"
+            />
+            <div className="search-item-data">
+              <div className="title">{result.Title}</div>
+              <div className="meta">{`${result.Type} | ${result.Year}`}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="chevron">
+        <ChevronRight />
+      </div>
+    </div>
+  )
+}
+
+SearchResults.propTypes = {
+  searchResults: PropTypes.shape({
+    Search: PropTypes.arrayOf(
+      PropTypes.shape({
+        imdbID: PropTypes.string,
+        Poster: PropTypes.string,
+        Title: PropTypes.string,
+        Type: PropTypes.string,
+        Year: PropTypes.string,
+      }),
+    ),
+  }),
+}
+
+export default SearchResults
