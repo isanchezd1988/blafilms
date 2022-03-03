b759aebdb81d15db1b329fd9d396a7e98dd7c80a
Andres Martin Angulo <monkeyandres@protonmail.com>
2022-03-03 14:04:14 +0100

    Add some E2E tests

---
+++ src/__tests__/e2e.test.js
@@ -0,0 +1,94 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import App from '../App'
+
+const SEARCH_QUERY = 'matrix'
+
+const MOVIE_TITLES_PER_PAGE = {
+  1: ['The Matrix', 'The Matrix Reloaded', 'The Matrix Revolutions'],
+  2: ['Sex and the Matrix', 'The Matrix Recalibrated'],
+  3: [],
+}
+
+const performSearch = async () => {
+  const searchInput = screen.getByPlaceholderText('Search...')
+  const performSearchButton = screen.getByText('Search', {
+    selector: 'button',
+  })
+
+  await userEvent.click(searchInput)
+  await userEvent.keyboard(SEARCH_QUERY)
+
+  await userEvent.click(performSearchButton)
+
+  await waitForElementToBeRemoved(() => screen.getByText('No results yet'))
+}
+
+describe('End to end App tests', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+
+  it('renders the app correctly the first time', () => {
+    expect(screen.getByPlaceholderText('Search...')).toBeVisible()
+    expect(screen.getByText('No results yet')).toBeVisible()
+  })
+
+  it('allows the user to perform searches with custom keywords', async () => {
+    const searchInput = screen.getByPlaceholderText('Search...')
+    const performSearchButton = screen.getByText('Search', {
+      selector: 'button',
+    })
+
+    expect(searchInput).toBeVisible()
+    expect(performSearchButton).toBeVisible()
+
+    await userEvent.click(searchInput)
+    await userEvent.keyboard(SEARCH_QUERY)
+    expect(searchInput.value).toBe(SEARCH_QUERY)
+
+    await userEvent.click(performSearchButton)
+
+    await waitForElementToBeRemoved(() => screen.getByText('No results yet'))
+
+    MOVIE_TITLES_PER_PAGE[1].forEach(movieTitle => {
+      expect(screen.getByText(movieTitle, { exact: true })).toBeVisible()
+    })
+  })
+
+  it('allows the user to navigate to different result page', async () => {
+    await performSearch()
+
+    const nextArrow = screen.getByLabelText('nextPage')
+    const prevArrow = screen.getByLabelText('prevPage')
+
+    await userEvent.click(nextArrow)
+
+    await waitForElementToBeRemoved(() =>
+      MOVIE_TITLES_PER_PAGE[1].map(movieTitle =>
+        screen.getByText(movieTitle, { exact: true }),
+      ),
+    )
+
+    MOVIE_TITLES_PER_PAGE[2].forEach(movieTitle => {
+      expect(screen.getByText(movieTitle, { exact: true })).toBeVisible()
+    })
+
+    await userEvent.click(prevArrow)
+
+    await waitForElementToBeRemoved(() =>
+      MOVIE_TITLES_PER_PAGE[2].map(movieTitle =>
+        screen.getByText(movieTitle, { exact: true }),
+      ),
+    )
+
+    MOVIE_TITLES_PER_PAGE[1].forEach(movieTitle => {
+      expect(screen.getByText(movieTitle, { exact: true })).toBeVisible()
+    })
+  })
+})
