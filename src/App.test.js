199ffe8072bcdd13ad92c60c0ddef404a9830d0f
Ivan Alcalde <ivan@redradix.com>
2022-03-06 18:17:44 +0100

    Add context when movies are not fetched yet
    - added describe for context when movies are not fetched yet
    - added test that indicates the status of waiting for movies
    - added TODO note for a potential refactor to do about `status role` and `semantics`

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,12 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('fails', () => {
-    render(<App />)
+  describe('when movies are not fetched yet from the API', () => {
+    it('indicates the status of waiting for the movies', () => {
+      render(<App />)
 
-    expect(screen.getByText('No results yet')).toBeInTheDocument()
+      // TODO: refactor semantics - https://www.w3.org/TR/wai-aria-1.1/#status
+      expect(screen.getByText('No results yet')).toBeInTheDocument()
+    })
   })
 })
