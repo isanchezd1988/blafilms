3f87c4555ee3b33198eed890e98eb39387ca4daa
Roberto Redradix <roberto@redradix.com>
2022-03-10 18:59:34 +0100

    Test with bounded queries from render instead of from global screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,25 @@
-import { render, screen, within } from '@testing-library/react'
+import { render, within } from '@testing-library/react'
 
 import App from './App'
 
 describe('App', () => {
   describe('when just mounted', () => {
     it('shows a default message', () => {
-      render(<App />)
+      const { getByText } = render(<App />)
 
-      expect(screen.getByText('No results yet')).toBeVisible()
+      expect(getByText('No results yet')).toBeVisible()
     })
 
     it("doesn't show a list of movies", () => {
-      render(<App />)
+      const { queryByRole } = render(<App />)
 
-      expect(screen.queryByRole('list')).toBeNull()
+      expect(queryByRole('list')).toBeNull()
     })
 
     it('shows an empty searchbox', () => {
-      render(<App />)
+      const { getByRole } = render(<App />)
 
-      const searchBox = screen.getByRole('searchbox')
+      const searchBox = getByRole('searchbox')
 
       expect(searchBox).toBeVisible()
       expect(searchBox.value).toBe('')
@@ -28,9 +28,9 @@ describe('App', () => {
 
   describe('when loaded', () => {
     it('shows a default movies list', async () => {
-      render(<App />)
+      const { findByRole } = render(<App />)
 
-      const movieList = await screen.findByRole('list')
+      const movieList = await findByRole('list')
 
       expect(movieList).toBeVisible()
       expect(within(movieList).getAllByRole('listitem').length).toBeGreaterThan(
